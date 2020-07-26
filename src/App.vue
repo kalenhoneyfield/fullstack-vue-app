eb08430e7a3e2c46c367a497b5b38f587224ce38
Kalen <kalenhoneyfield@gmail.com>
2020-07-26 10:21:10 -0400

    Add bootstrap and bootstrap theme and NavBar
    Added a bootstrap theme from themestr.app, will want to work on
    implementing sass into the project to better handle custom themeing

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
   <div id="app">
+    <NavBar />
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import NavBar from '@/components/NavBar.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    NavBar,
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
