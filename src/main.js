39eda0d9a72525f67571ecaae0b0a624be1bdcea
Kalen <kalenhoneyfield@gmail.com>
2020-08-14 20:28:43 -0400

    Move to Sass styling
    Added sass and removed the old custom css file. Converted the the old
    css to scss and sass

--- src/main.js
+++ src/main.js
@@ -5,9 +5,10 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
-import 'bootstrap/dist/css/bootstrap.css';
-import './assets/theme_1595771679244.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import './assets/theme_1595771679244.css';
+// import 'bootstrap-vue/dist/bootstrap-vue.css';
+import './assets/sass/index.sass';
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
